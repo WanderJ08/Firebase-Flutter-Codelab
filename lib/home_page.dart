59c4724b88b0b7434ee59619c638035e1ad6d7a5
WanderJ08 <wanderroque01@gmail.com>
2024-11-16 14:59:50 -0400

    refactor: remove duplicate import

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,6 @@ import 'guest_book.dart'; // new
 import 'app_state.dart'; // new
 import 'src/authentication.dart'; // new
 import 'src/widgets.dart';
-import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
